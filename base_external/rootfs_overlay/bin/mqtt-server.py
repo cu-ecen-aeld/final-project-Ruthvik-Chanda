a78dc559a71222ace95ec181ad3579b246d0f7e2
ruch7213 <ruch7213@colorado.edu>
2023-04-30 01:32:57 +0000

    Update - Buildroot

--- base_external/rootfs_overlay/bin/mqtt-server.py
+++ base_external/rootfs_overlay/bin/mqtt-server.py
@@ -1,7 +1,9 @@
 #Filename: Server-Subscriber.py
-#Auther: Sankalp Pund
+#Auther: Ruthvik R Chanda
 #Description: This python script works as mqtt server-subscriber
 #Reference: https://github.com/eclipse/paho.mqtt.python
+#         : https://github.com/cu-ecen-aeld/buildroot-assignments-base/wiki/MQTT-Python-Example
+#         : https://github.com/cu-ecen-aeld/buildroot-assignments-base/tree/9648e6530170935479d6e8cd86a845a177529c0f/base_external/rootfs_overlay/bin/MQTT
 
 #Importing Modules
 try:
@@ -18,7 +20,7 @@ Port=1883
 Keepalive=60
 try:
 	# This is the Subscriber
-	print("I am Server")
+	print("Air Quality Monitoring Server")
 
 	#Function Defination: https://github.com/eclipse/paho.mqtt.python/blob/1eec03edf39128e461e6729694cf5d7c1959e5e4/src/paho/mqtt/subscribe.py#L26
 	def on_connect(client, userdata, flags, rc):
@@ -28,7 +30,7 @@ try:
 	#Function Defination: https://github.com/eclipse/paho.mqtt.python/blob/4f53d64675e4d1e9c78ce9fb2e450ba717dad3da/examples/client_sub.py#L28  
 	def on_message(client, userdata, msg):
 	  recieved_payload=msg.payload.decode()
-	  print("Received Message from Client is - - ->: '%s'" %recieved_payload)	
+	  print("'%s'" %recieved_payload)	
 	  if recieved_payload == "Sending test data to Server...":
 	    print("Success! Test data has been received at server from client side")
 	
